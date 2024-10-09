39df1e3b38ad21e173319cd90c55b87f47c7ae96
AdamBrown04 <adambrown2004@outlook.com>
2024-10-09 13:19:36 +0100

    Add project files.

---
+++ Y2WK3-Essential/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.WriteLine("What porgram do you want to run? \n1)Username with underscore \n2)Check if number is between 0 and 256");
+int option = Convert.ToInt32(Console.ReadLine());
+
+switch (option)
+{
+    case 1:
+        List<string> usernames = new List<string>();
+
+        Console.WriteLine("Enter END when you want the program to stop");
+        while (true)
+        {
+            Console.Write("Enter a username: ");
+            string usernameInput = Console.ReadLine();
+
+            if (usernameInput.ToUpper() == "END")
+            {
+                break;
+            }
+            else
+            {
+                usernames.Add(usernameInput);
+            }
+        }
+
+        IEnumerable<string> usersWithUnderscore = usernames.Where(username => username.Contains("_"));
+        foreach (string username in usersWithUnderscore)
+        {
+            Console.WriteLine(username); 
+        }
+        break;
+    case 2:
+        List<int> listOfNumbers = new List<int>();
+
+        Console.WriteLine("Enter END when you want the program to stop");
+        while (true)
+        {
+            Console.Write("Enter a number: ");
+            string numberInput = Console.ReadLine();
+
+            if(numberInput.ToUpper() == "END")
+            {
+
+            }
+        }
+        break;
+}
